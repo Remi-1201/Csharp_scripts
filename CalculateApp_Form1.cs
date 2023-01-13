fd1376609a9dc7d6aa7f7a07b89a0432e13b708c
Remi <97021497+Remi-1201@users.noreply.github.com>
2023-01-13 15:26:39 +0900

    CalculateApp_Form1.cs

---
+++ CalculateApp_Form1.cs
@@ -0,0 +1,16 @@
+namespace Calculator
+{
+  public partial class From1 : Form
+  {
+    InitializeComponent();
+  }
+  
+  private void button1_Click(object sender, EventArgs e)
+  {
+    // ➊ int型の変数５個を宣言
+    int price, quantity, subtotal, tax, total;
+    // ➋　消費税率0.1を定数TAX_RATEに格納
+    const double TAX_RATE = 0.1;
+    
+  }
+}
